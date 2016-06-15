7f4bb8019c380d6ee0daf4b0e0b8b0a4ae2d3a42
bmcculley <bmcculley@outlook.com>
2016-06-15 16:44:22 -0400

    guideline updates

--- footer.php
+++ footer.php
@@ -49,7 +49,7 @@
 				?>
 				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
 				<span><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a></span>
-				<a href="<?php echo esc_url( __( 'http://mkdir.info/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Twenty Sixteen child theme by %s', 'twentysixteen' ), 'bmcculley' ); ?></a>
+				<a href="<?php echo esc_url( __( 'http://mkdir.info/twenty-sixteen-child', 'twentysixteen' ) ); ?>"><?php printf( __( 'Twenty Sixteen child theme by %s', 'twentysixteen' ), 'bmcculley' ); ?></a>
 			</div><!-- .site-info -->
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
